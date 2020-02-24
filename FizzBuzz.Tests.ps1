c53fd627977bf5103fa41534d712be8823ca8331
masato <mutsuya_masato@yahoo.co.jp>
2020-02-24 16:15:41 +0900

    テストコードのContextを追記

--- FizzBuzz.Tests.ps1
+++ FizzBuzz.Tests.ps1
@@ -2,17 +2,19 @@
 $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
-Describe "FizzBuzz" {
-    It "3,5の倍数以外はそのまま" {
-        FizzBuzz -Number 1 | Should Be '1'
-    }
-    It "3の倍数ならFizz"{
-        FizzBuzz -Number 3 | Should Be 'Fizz'
-    }
-    It "5の倍数ならBuzz"{
-        FizzBuzz -Number 5 | Should Be 'Buzz'
-    }
-    It "3と5の倍数ならFizzBuzz"{
-        FizzBuzz -Number 15 | Should Be 'FizzBuzz'
+Describe "FizzBuzz関数の動作確認" {
+    Context "TDD" {
+        It "3,5の倍数以外はそのまま" {
+            FizzBuzz -Number 1 | Should Be '1'
+        }
+        It "3の倍数ならFizz"{
+            FizzBuzz -Number 3 | Should Be 'Fizz'
+        }
+        It "5の倍数ならBuzz"{
+            FizzBuzz -Number 5 | Should Be 'Buzz'
+        }
+        It "3と5の倍数ならFizzBuzz"{
+            FizzBuzz -Number 15 | Should Be 'FizzBuzz'
+        }
     }
 }
