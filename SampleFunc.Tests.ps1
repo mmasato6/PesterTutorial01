3cc3bb12e7a118736d3ec1aa0473a5c24fb5b9e7
masato <mutsuya_masato@yahoo.co.jp>
2020-02-24 15:56:47 +0900

    実装してみた

--- SampleFunc.Tests.ps1
+++ SampleFunc.Tests.ps1
@@ -3,7 +3,13 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
 . "$here\$sut"
 
 Describe "SampleFunc" {
-    It "does something useful" {
-        $true | Should Be $false
+    It "1を与えたら2が返る" {
+        SampleFunc 1 | Should Be 2
+    }
+    It "2を与えたら4が返る" {
+        SampleFunc 2 | Should Be 4
+    }
+    It "0を与えたら0が返る" {
+        SampleFunc 0 | Should Be 0
     }
 }
--- SampleFunc.ps1
+++ SampleFunc.ps1
@@ -1,3 +1,3 @@
-﻿function SampleFunc {
-
+﻿function SampleFunc([int]$i) {
+    return $i * 2
 }
