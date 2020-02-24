3bba9dab2376a14d9387350eb098e04734211b16
masato <mutsuya_masato@yahoo.co.jp>
2020-02-24 16:31:22 +0900

    elseifを早期リターンに変更

--- FizzBuzz.ps1
+++ FizzBuzz.ps1
@@ -1,13 +1,12 @@
 ﻿function FizzBuzz([int]$number) {
     if ($number % 15 -eq 0) {
         return 'FizzBuzz'
-    } elseif (($number % 3) -eq 0) {
+    } 
+    if (($number % 3) -eq 0) {
         return 'Fizz'    
-    } elseif (($number % 5 -eq 0)) {
+    } 
+    if (($number % 5 -eq 0)) {
         return 'Buzz'
-    } else {
-        return $number    
-    }
-    
-    
+    } 
+    return $number    
 }
